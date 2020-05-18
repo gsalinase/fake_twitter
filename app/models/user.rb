d63e532ed0be527edabc7db2af47e9f0d97459e0
Gonzalo Salinas <gsalinase91@gmail.com>
2020-05-17 23:18:40 -0400

    Add: default image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :tweets
   has_many :likes
+  attribute :image_url, :string, default: 'https://gcdn.net/wp-content/uploads/2018/10/default-avatar.png'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
