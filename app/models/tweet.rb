96e1cdeb64e38d3449e62dc5a0eac9215b9b4082
Gonzalo Salinas <gsalinase91@gmail.com>
2020-05-17 23:04:59 -0400

    Fix: default into tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,5 +3,7 @@ class Tweet < ApplicationRecord
   has_many :likes
   has_many :retweets
   validates :content, presence: true
+  attribute :total_like, :integer, default: 0
+  attribute :total_retweet, :integer, default: 0
   paginates_per 50
 end
